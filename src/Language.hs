af36fb616035492facc576a6fc12a9d225005955
cutsea110 <cutsea110@gmail.com>
2021-07-24 15:40:11 +0900

    pretty print

--- src/Language.hs
+++ src/Language.hs
@@ -68,3 +68,125 @@ preludeDefs
     , ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f"))
     ]
   
+-- pretty printer
+pprint :: CoreProgram -> String
+pprint prog = iDisplay (pprProgram prog)
+
+{--
+pprExpr :: CoreExpr -> String
+pprExpr (ENum n)    = show n
+pprExpr (EVar v)    = v
+pprExpr (EAp e1 e2) = pprExpr e1 ++ " " ++ pprExpr e2
+
+pprAExpr :: CoreExpr -> String
+pprAExpr e
+  | isAtomicExpr e = pprExpr e
+  | otherwise      = "(" ++ pprExpr e ++ ")"
+
+mkMultiAp :: Int -> CoreExpr -> CoreExpr -> CoreExpr
+mkMultiAp n e1 e2 = foldl EAp e1 $ replicate n e2
+--}
+
+class Iseq iseq where
+  iNil :: iseq
+  iStr :: String -> iseq
+  iAppend :: iseq -> iseq -> iseq
+  iNewline :: iseq
+  iIndent :: iseq -> iseq
+  iDisplay :: iseq -> String
+
+infixr 5 `iAppend`
+
+data Iseqrep = INil
+             | IStr String
+             | IAppend Iseqrep Iseqrep
+
+instance Iseq Iseqrep where
+  iNil         = INil
+  iStr         = IStr
+  iAppend      = IAppend
+  iNewline     = IStr "\n"
+  iIndent seq  = seq
+  iDisplay seq = flatten [seq]
+
+flatten :: [Iseqrep] -> String
+flatten [] = ""
+flatten (INil : seqs) = flatten seqs
+flatten (IStr s : seqs) = s ++ flatten seqs
+flatten (IAppend seq1 seq2 : seqs) = flatten (seq1 : seq2 : seqs)
+
+pprProgram :: CoreProgram -> Iseqrep
+pprProgram scdefns = iInterleave iNL $ map pprScDefn scdefns
+
+pprScDefn :: CoreScDefn -> Iseqrep
+pprScDefn (name, args, expr)
+  = iConcat [ iStr name, iSpace, pprArgs args
+            , iStr " = "
+            , iIndent (pprExpr expr)
+            ]
+
+iSpace :: Iseq iseq => iseq
+iSpace = iStr " "
+
+pprArgs :: [String] -> Iseqrep
+pprArgs args = iInterleave iSpace $ map iStr args
+
+pprExpr :: CoreExpr -> Iseqrep
+pprExpr (ENum n) = iStr (show n)
+pprExpr (EVar v) = iStr v
+pprExpr (EConstr tag arity)
+  = iConcat [ iStr "Pack{"
+            , iStr (show tag), iStr ",", iStr (show arity)
+            , iStr "}"
+            ]
+pprExpr (EAp e1 e2)
+  = iConcat [ pprExpr e1, iStr " ", pprAExpr e2 ]
+pprExpr (ELet isrec defns expr)
+  = iConcat [ iStr keyword, iNewline
+            , iStr " ", iIndent (pprDefns defns), iNewline
+            , iStr "in ", pprExpr expr
+            ]
+  where
+    keyword | not isrec = "let"
+            | isrec     = "letrec"
+pprExpr (ECase e alts)
+  = iConcat [ iStr "case ", iIndent (pprExpr e), iStr " of", iNewline
+            , iStr "  ", iIndent (iInterleave iNL (map pprAlt alts))
+            ]
+
+pprExpr (ELam args e)
+  = iConcat [ iStr "\\ ", pprArgs args, iStr " -> "
+            , iIndent (pprExpr e)
+            ]
+
+iNL :: Iseq iseq => iseq
+iNL = iStr " ;" `iAppend` iNewline
+
+pprAlt :: CoreAlter -> Iseqrep
+pprAlt (i, args, expr)
+  = iConcat [ iStr "<", iStr (show i), iStr ">", iSpace, pprArgs args
+            , iStr " -> ", iIndent (pprExpr expr)
+            ]
+
+pprAExpr :: CoreExpr -> Iseqrep
+pprAExpr e
+  | isAtomicExpr e = pprExpr e
+  | otherwise      = iStr "(" `iAppend` pprExpr e `iAppend` iStr ")"
+
+pprDefns :: [(Name, CoreExpr)] -> Iseqrep
+pprDefns defns = iInterleave sep (map pprDefn defns)
+  where
+    sep = iConcat [ iStr ";", iNewline ]
+
+pprDefn :: (Name, CoreExpr) -> Iseqrep
+pprDefn (name, expr)
+  = iConcat [ iStr name, iStr " = ", iIndent (pprExpr expr) ]
+
+iConcat :: Iseq iseq => [iseq] -> iseq
+iConcat = foldr iAppend iNil
+
+iInterleave :: Iseq iseq => iseq -> [iseq] -> iseq
+iInterleave sep [] = iNil
+iInterleave sep [x] = x
+iInterleave sep (x:xs)
+  = x `iAppend` sep `iAppend` iInterleave sep xs
