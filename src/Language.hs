c0e01b9d2ce0334346e04c97933d9d96572241cd
cutsea110 <cutsea110@gmail.com>
2021-08-22 13:27:02 +0900

    cosme

--- src/Language.hs
+++ src/Language.hs
@@ -10,7 +10,7 @@ import Utils
 data Expr a
   = EVar Name
   | ENum Int
-  | EConstr Int Int -- ^ tag and arity
+  | EConstr Tag Arity
   | EAp (Expr a) (Expr a)
   | ELet IsRec [(a, Expr a)] (Expr a)
   | ECase (Expr a) [Alter a]
