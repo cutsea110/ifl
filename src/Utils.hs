941f49a1c8619307b8138d939d6f944baf040d9a
cutsea110 <cutsea110@gmail.com>
2021-10-01 05:34:29 +0900

    add tests

--- src/Utils.hs
+++ src/Utils.hs
@@ -18,6 +18,17 @@ space :: Int -> String
 space n = replicate n ' '
 
 type Assoc a b = [(a, b)]
+
+{- |
+>>> aLookup [] 1 "missing"
+"missing"
+
+>>> aLookup [(1, "found")] 1 "not found"
+"found"
+
+>>> aLookup [(1, "found"), (2, "ok")] 2 "not found"
+"ok"
+-}
 aLookup :: Eq k => Assoc k v -> k -> v -> v
 aLookup [] _ d = d
 aLookup ((k, v):kvs) k' d
