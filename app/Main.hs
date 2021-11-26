1f5ab439fbf43483dec19e3b8ffd19b8a2ffe4df
cutsea110 <cutsea110@gmail.com>
2021-11-26 18:49:46 +0900

    refactor main module

--- app/Main.hs
+++ app/Main.hs
@@ -1,48 +1,28 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.IO (getContents)
+
 -- import Template.Mark1 (parse, compile, eval, showResults)
 -- import Template.Mark2 (parse, compile, eval, showResults)
+-- import Template.Mark3 (parse, compile, eval, showResults)
 import Template.Mark4 (parse, compile, eval, showResults)
 
-testProg0, testProg1, testProg2, testProg3, testProg4 :: String
-testProg0 = "main = S K K 3"
-testProg1 = "main = S K K"
-testProg2 = unlines [ "id = S K K ;"
-                    , "main = twice twice twice id 3"
-                    ]
-testProg3 = unlines [ "pair x y f = f x y ;"
-                    , "fst p = p K ;"
-                    , "snd p = p K1 ;"
-                    , "f x y = letrec"
-                    , "           a = pair x b ;"
-                    , "           b = pair y a"
-                    , "        in"
-                    , "        fst (snd (snd (snd a))) ;"
-                    , "main = f 3 4"
-                    ]
+run :: String -> IO ()
+run = putStrLn . showResults . eval . compile . parse
 
-testProg4 = unlines [ "id x = x ;"
-                    , "main = twice twice twice id 3"
-                    ]
+printHelp :: IO ()
+printHelp = putStrLn "cabal v2-run ifl <file-path>"
 
-testProg5 = unlines [ "main = negate (negate 7)"
-                    ]
-
-testProg6 = unlines [ "main = ((6 / 2) * 6) + (4 * (10 - 4))"
-                    ]
-
-testProg7 = unlines [ "double x = x + x ;"
-                    , "main = S K K double (double 3)"
-                    ]
-
-testProg8 = unlines [ "double x = x + x ;"
-                    , "main = double (S K K 3)"
-                    ]
-
-test :: String -> IO ()
-test = putStrLn . showResults . eval . compile . parse
+exec :: FilePath -> IO ()
+exec file = do
+  putStrLn $ "Program Source: " ++ file
+  testProg <- readFile file
+  -- compile and eval program
+  run testProg
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  test testProg8
+  args <- getArgs
+  if null args then printHelp
+    else exec (head args)
