cba4eed87c633187d64441bb5e71c2d4f0cd5b0e
cutsea110 <cutsea110@gmail.com>
2021-11-26 17:21:01 +0900

    use filepath

--- test/doctests.hs
+++ test/doctests.hs
@@ -2,8 +2,9 @@ module Main where
 
 import Test.DocTest
 import System.Directory
+import System.FilePath ((</>))
 
 main :: IO ()
 main = do
   fs <- listDirectory "src"
-  doctest $ "-isrc" : map ("src/"++) fs
+  doctest $ "-isrc" : map ("src" </>) fs
