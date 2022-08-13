fd5a4cced58339da4701e880b7381a4e34cdfd9c
cutsea110 <cutsea110@gmail.com>
2022-08-13 16:17:56 +0900

    udpate

--- src/Gmachine/Mark1.hs
+++ src/Gmachine/Mark1.hs
@@ -4,15 +4,11 @@ import Heap
 import Iseq
 import Language
 import Parser
-import Stack
+import qualified Stack as S
 import Utils
 
 runProg :: String -> String
 runProg = showResults . eval . compile . parse
-
-showResults = undefined
-eval = undefined
-compile = undefined
 
 data GmState = GmState { code    :: GmCode
                        , stack   :: GmStack
@@ -34,10 +30,11 @@ data Instruction
   | Pushglobal Name
   | Pushint Int
   | Mkap
+  | Push Int -- push offset
   | Slide Int
   deriving (Eq)
 
-type GmStack = Stack Addr
+type GmStack = S.Stack Addr
 
 getStack :: GmState -> GmStack
 getStack state = stack state
@@ -83,3 +80,73 @@ getStats state = stats state
 
 putStats :: GmStats -> GmState -> GmState
 putStats stats' state = state { stats = stats' }
+
+
+
+eval :: GmState -> [GmState]
+eval state = state : restStates
+  where
+    restStates | gmFinal state = []
+               | otherwise     = eval nextState
+    nextState  = doAdmin (step state)
+
+
+doAdmin :: GmState -> GmState
+doAdmin s = putStats (statIncSteps (getStats s)) s
+
+gmFinal :: GmState -> Bool
+gmFinal s = null $ getCode s
+
+step :: GmState -> GmState
+step state = dispatch i (putCode is state)
+  where i:is = getCode state
+
+dispatch :: Instruction -> GmState -> GmState
+dispatch (Pushglobal f) = pushglobal f
+dispatch (Pushint n)    = pushint n
+dispatch Mkap           = mkap
+dispatch (Push n)       = push n
+dispatch (Slide n)      = slide n
+dispatch Unwind         = unwind
+
+pushglobal :: Name -> GmState -> GmState
+pushglobal f state = putStack (S.push a $ getStack state) state
+  where a = aLookup (getGlobals state) f (error $ "Undeclared global " ++ f)
+
+pushint :: Int -> GmState -> GmState
+pushint n state = putHeap heap' (putStack (S.push a $ getStack state) state)
+  where (heap', a) = hAlloc (getHeap state) (NNum n)
+
+mkap :: GmState -> GmState
+mkap state = putHeap heap' (putStack (S.push a s2) state)
+  where (heap', a)  = hAlloc (getHeap state) (NAp a1 a2)
+        (a1, s1) = S.pop $ getStack state
+        (a2, s2) = S.pop s1
+
+push :: Int -> GmState -> GmState
+push n state = putStack (S.push a s) state
+  where s = getStack state
+        a = getArg (hLookup (getHeap state) (S.getStack s !! (n+1)))
+
+getArg :: Node -> Addr
+getArg (NAp _ a2) = a2
+getArg _          = error "not application Node"
+
+slide :: Int -> GmState -> GmState
+slide n state = putStack (S.push a $ S.discard n s) state
+  where (a, s) = S.pop $ getStack state
+
+unwind :: GmState -> GmState
+unwind state = newState (hLookup heap a)
+  where s = getStack state
+        (a, s1) = S.pop s
+        heap   = getHeap state
+        newState (NNum n) = state
+        newState (NAp a1 a2) = putCode [Unwind] (putStack (S.push a1 s) state)
+        newState (NGlobal n c)
+          | S.getDepth s1 < n = error "Unwinding with too few arguments"
+          | otherwise         = putCode c state
+
+showResults = undefined
+compile = undefined
+
