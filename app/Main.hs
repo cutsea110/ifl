bf428358158f8966095c330cefa9aabf178f18d1
cutsea110 <cutsea110@gmail.com>
2023-03-01 21:11:17 +0900

    refine

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Monad (forM_)
+import Data.Char (toLower)
 import Data.List (intercalate)
 import Data.Maybe (fromMaybe)
 import System.Console.GetOpt
@@ -31,27 +32,27 @@ import qualified Gmachine.Mark6 as GMark6 (runProg)
 type Executer = String -> IO ()
 
 executer :: Compiler -> Executer
-executer Mark1 = putStrLn . Mark1.runProg
-executer Mark2 = putStrLn . Mark2.runProg
-executer Mark3 = putStrLn . Mark3.runProg
-executer Mark4 = putStrLn . Mark4.runProg
-executer Mark5 = putStrLn . Mark5.runProg
-executer Mark5cnv = putStrLn . Mark5.runProgWithConv
-executer Mark5Alt = putStrLn . Mark5Alt.runProg
-executer Mark5Altcnv = putStrLn . Mark5Alt.runProgWithConv
-executer Mark5GC = putStrLn . Mark5GC.runProg
-executer Mark5GCcnv = putStrLn . Mark5GC.runProgWithConv
-executer Mark5RevGC = putStrLn . Mark5RevGC.runProg
+executer Mark1         = putStrLn . Mark1.runProg
+executer Mark2         = putStrLn . Mark2.runProg
+executer Mark3         = putStrLn . Mark3.runProg
+executer Mark4         = putStrLn . Mark4.runProg
+executer Mark5         = putStrLn . Mark5.runProg
+executer Mark5cnv      = putStrLn . Mark5.runProgWithConv
+executer Mark5Alt      = putStrLn . Mark5Alt.runProg
+executer Mark5Altcnv   = putStrLn . Mark5Alt.runProgWithConv
+executer Mark5GC       = putStrLn . Mark5GC.runProg
+executer Mark5GCcnv    = putStrLn . Mark5GC.runProgWithConv
+executer Mark5RevGC    = putStrLn . Mark5RevGC.runProg
 executer Mark5RevGCcnv = putStrLn . Mark5RevGC.runProgWithConv
-executer Mark5Cp = putStrLn . Mark5Cp.runProg
-executer Mark5Cpcnv = putStrLn . Mark5Cp.runProgWithConv
-executer GMark1 = putStrLn . GMark1.runProg
-executer GMark2 = putStrLn . GMark2.runProg
-executer GMark3 = putStrLn . GMark3.runProg
-executer GMark4 = putStrLn . GMark4.runProg
-executer GMark5 = putStrLn . GMark5.runProg
-executer GMark6 = putStrLn . GMark6.runProg
-executer (Noco name) = \_ -> do
+executer Mark5Cp       = putStrLn . Mark5Cp.runProg
+executer Mark5Cpcnv    = putStrLn . Mark5Cp.runProgWithConv
+executer GMark1        = putStrLn . GMark1.runProg
+executer GMark2        = putStrLn . GMark2.runProg
+executer GMark3        = putStrLn . GMark3.runProg
+executer GMark4        = putStrLn . GMark4.runProg
+executer GMark5        = putStrLn . GMark5.runProg
+executer GMark6        = putStrLn . GMark6.runProg
+executer (Noco name)   = \_ -> do
   putStrLn $ "Error: Unknown compiler = " ++ name
   printHelp
 
@@ -59,28 +60,13 @@ executer (Noco name) = \_ -> do
 -- COMMAND LINE OPTIONS
 ---------------------------------------------------------------
 
-data Compiler = Noco String
-              | Mark1
-              | Mark2
-              | Mark3
-              | Mark4
-              | Mark5
-              | Mark5cnv
-              | Mark5Alt
-              | Mark5Altcnv
-              | Mark5GC
-              | Mark5GCcnv
-              | Mark5RevGC
-              | Mark5RevGCcnv
-              | Mark5Cp
-              | Mark5Cpcnv
-              | GMark1
-              | GMark2
-              | GMark3
-              | GMark4
-              | GMark5
-              | GMark6
-              deriving Show
+data Compiler
+  = Noco String
+  | Mark1 | Mark2 | Mark3 | Mark4
+  | Mark5 | Mark5cnv | Mark5Alt | Mark5Altcnv | Mark5GC | Mark5GCcnv
+  | Mark5RevGC | Mark5RevGCcnv | Mark5Cp | Mark5Cpcnv
+  | GMark1 | GMark2 | GMark3 | GMark4 | GMark5 | GMark6
+  deriving Show
 
 data Options = Options
   { optVerbose     :: Bool -- TODO
@@ -96,26 +82,13 @@ defaultOptions = Options
   }
 
 name2Compiler :: [(String, Compiler)]
-name2Compiler = [ ("mark1", Mark1)
-                , ("mark2", Mark2)
-                , ("mark3", Mark3)
-                , ("mark4", Mark4)
-                , ("mark5", Mark5)
-                , ("mark5cnv", Mark5cnv)           -- convert newer version
-                , ("mark5alt", Mark5Alt)
-                , ("mark5altcnv", Mark5Altcnv)     -- convert newer version
-                , ("mark5gc", Mark5GC)             -- mark gc
-                , ("mark5gccnv", Mark5GCcnv)       -- convert newer version
-                , ("mark5revgc", Mark5RevGC)       -- pointer reversal mark gc
-                , ("mark5revgccnv", Mark5RevGCcnv) -- convert newer version
-                , ("mark5cp", Mark5Cp)             -- convert newer version
-                , ("gmark1", GMark1)               -- Gmachine
-                , ("gmark2", GMark2)               -- Gmachine
-                , ("gmark3", GMark3)               -- Gmachine
-                , ("gmark4", GMark4)               -- Gmachine
-                , ("gmark5", GMark5)               -- Gmachine
-                , ("gmark6", GMark6)               -- Gmachine
-                ]
+name2Compiler
+  = map (\c -> (map toLower (show c), c))
+    [ Mark1, Mark2, Mark3, Mark4
+    , Mark5, Mark5cnv, Mark5Alt, Mark5Altcnv, Mark5GC, Mark5GCcnv
+    , Mark5RevGC, Mark5RevGCcnv, Mark5Cp
+    , GMark1, GMark2, GMark3, GMark4, GMark5, GMark6
+    ]
 
 compilerNames :: [String]
 compilerNames = map fst name2Compiler
