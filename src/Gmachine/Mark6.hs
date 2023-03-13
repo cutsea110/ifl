d6db81f03c37616257ed8fd6f9f8de7d2a860e61
cutsea110 <cutsea110@gmail.com>
2023-03-13 12:04:46 +0900

    fix: letrec

--- src/Gmachine/Mark6.hs
+++ src/Gmachine/Mark6.hs
@@ -774,14 +774,12 @@ compileArgs defs env
 -}
 compileLetrec :: GmCompiler -> [(Name, CoreExpr)] -> GmCompiler
 compileLetrec comp defs expr env
-  = [Alloc n] ++ compileLetrec' defs env' ++ comp expr env' ++ [Slide n]
+  = [Alloc n] ++ compiled defs (n-1) ++ comp expr env' ++ [Slide n]
   where n = length defs
         env' = compileArgs defs env
-
-compileLetrec' :: [(Name, CoreExpr)] -> GmEnvironment -> GmCode
-compileLetrec' []                  env = []
-compileLetrec' ((name, expr):defs) env
-  = compileC expr env ++ [Update (length defs)]
+        compiled dds i = case dds of
+          [] -> []
+          d:ds -> compileC (snd d) env' ++ [Update i] ++ compiled ds (i-1)
 
 argOffset :: Int -> GmEnvironment -> GmEnvironment
 argOffset n env = [(v, n+m) | (v, m) <- env]
