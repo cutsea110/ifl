ac497c89a1bf0fb04316282c89eea21dc34fe0ed
cutsea110 <cutsea110@gmail.com>
2023-07-24 10:01:45 +0900

    fix: stack depth stats on Mark5

--- src/Gmachine/Mark5.hs
+++ src/Gmachine/Mark5.hs
@@ -55,7 +55,9 @@ getStack :: GmState -> GmStack
 getStack state = stack state
 
 putStack :: GmStack -> GmState -> GmState
-putStack stack' state = state { stack = stack' }
+putStack stack' state = state { stack = stack', stats = stats' }
+  where dwh = S.getHighWaterMark stack'
+        stats' = statUpdateMaxStackDepth dwh (getStats state)
 
 type GmDump = S.Stack GmDumpItem
 type GmDumpItem = (GmCode, GmStack)
@@ -90,16 +92,29 @@ getGlobals state = globals state
 putGlobals :: GmGlobals -> GmState -> GmState
 putGlobals globals' state = state { globals = globals' }
 
-type GmStats = Int
+data GmStats = GmStats { getSteps         :: Int
+                       , getMaxStackDepth :: Int
+                       }
 
 statInitial :: GmStats
-statInitial = 0
+statInitial = GmStats { getSteps         = 0
+                      , getMaxStackDepth = 0
+                      }
 
 statIncSteps :: GmStats -> GmStats
-statIncSteps s = s + 1
+statIncSteps s = s { getSteps = getSteps s + 1 }
 
 statGetSteps :: GmStats -> Int
-statGetSteps s = s
+statGetSteps s = getSteps s
+
+statGetMaxStackDepth :: GmStats -> Int
+statGetMaxStackDepth s = getMaxStackDepth s
+
+statUpdateMaxStackDepth :: Int -> GmStats -> GmStats
+statUpdateMaxStackDepth depth s
+  | depth > depth' = s { getMaxStackDepth = depth }
+  | otherwise      = s
+  where depth' = statGetMaxStackDepth s
 
 getStats :: GmState -> GmStats
 getStats state = stats state
@@ -571,7 +586,7 @@ showStats s = iConcat [ iStr "---------------"
                       , iNewline, iStr "            Heap size = "
                       , iNum (hSize (heap s))
                       , iNewline, iStr "           Stack size = "
-                      , iNum (S.getHighWaterMark (getStack s))
+                      , iNum (statGetMaxStackDepth (getStats s))
                       ]
 
 {- |
